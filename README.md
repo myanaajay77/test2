658374febfdcb3b282fb0dc50e127428687919b5
myanaajay77 <myana.ajay77@gmail.com>
2019-08-23 11:20:42 +0530

    commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test2
-ListPython
+TestTupplePython
+
---
+++ testtupple.py
@@ -0,0 +1,8 @@
+i = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+for a in i:
+    if(a%2==0):
+        print ("even number is", a)
+    else:
+        print ("other number is",a)
+
+
